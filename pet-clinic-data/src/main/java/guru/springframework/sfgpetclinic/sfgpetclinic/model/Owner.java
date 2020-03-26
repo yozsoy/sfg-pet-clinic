748373fe1593666b06b22dc57ab6ba73033a3c82
y00562359 <ylozsoy@gmail.com>
2020-03-26 17:37:53 +0300

    48) Refactoring to multi module maven project. 2 modules are added, their pom updated manually. The previous pom alseo updated manually. Classes and packages refactored. closes #1

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinic.sfgpetclinic.model;
+
+public class Owner extends  Person {
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/sfgpetclinic/model/Person.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgpetclinic.model;
+package guru.springframework.sfgpetclinic.sfgpetclinic.model;
 
 public class Person {
     private String firstName;
