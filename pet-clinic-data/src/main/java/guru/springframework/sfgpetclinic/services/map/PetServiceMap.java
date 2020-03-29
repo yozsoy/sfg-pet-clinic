dbc7f16149c9a24183314565115edff80b2eadde
y00562359 <ylozsoy@gmail.com>
2020-03-29 18:43:40 +0300

    110) Static Resources has been Copied From https://github.com/springframeworkguru/sfg-pet-clinic.git. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -20,7 +20,7 @@ public class PetServiceMap  extends AbstractMapService<Pet, Long> implements Pet
 
     @Override
     public Pet save(Pet pet) {
-        return super.save(pet.getId(), pet);
+        return super.save(pet);
     }
 
     @Override
