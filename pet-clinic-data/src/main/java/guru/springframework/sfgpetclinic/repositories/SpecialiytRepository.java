e95ac723016c04dd2d19b34084aa8787c4adda6e
y00562359 <ylozsoy@gmail.com>
2020-03-31 20:44:49 +0300

    149) Implemented repo. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialiytRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialiytRepository extends CrudRepository<Speciality, Long> {
+}
